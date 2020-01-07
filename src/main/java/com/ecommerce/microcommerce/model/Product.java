5bf3799130f606ce2b01dcdae19f7a1b8a8e47e4
gregoire.jacquin <gregoire.jacquin@groupe-deya.com>
2020-01-07 14:40:59 +0100

    Finish microservice

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -3,10 +3,12 @@ package com.ecommerce.microcommerce.model;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 
 //@JsonIgnoreProperties(value = {"prixAchat","id"}) Pour ne pas etre visible lors de la réponse JSON
 //@JsonFilter("monFiltre")
@@ -15,7 +17,9 @@ public class Product {
     @Id
     @GeneratedValue
     private int id;
+    @Length(min = 3, max = 20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
+    @Min(value = 1)
     private int prix;
 
     //@JsonIgnore si seulement un a caché lors de la réponse
--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,18 +2,24 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@Api(description = "API pour les opérations CRUD sur les produits.")
 @RestController
 public class ProductController {
     @Autowired
@@ -29,9 +35,13 @@ public class ProductController {
         return produitsFiltres;
 
     }
+    @ApiOperation(value = "Récupère un produit grâce à son ID à condition que celui-ci soit en stock !")
     @GetMapping(value = "/Produits/{id}")
-    public Product affciherUnProduit(@PathVariable int id){
-        return productDao.findById(id);
+    public Product affciherUnProduit(@PathVariable int id) {
+        Product produit = productDao.findById(id);
+        if(produit == null)
+            throw new ProduitIntrouvableException("Le produit avec l'id" + id+ " est INTROUVABLE.");
+        return produit;
     }
     @GetMapping(value = "test/produits/{prixLimit}")
     public List<Product> testeDeRequetes(@PathVariable int prixLimit) {
@@ -42,7 +52,7 @@ public class ProductController {
         return productDao.findByNomLike("%"+recherche+"%");
     }
     @PostMapping(value = "/Produits")
-    public ResponseEntity<Void> ajouterProduit(@RequestBody Product product) {
+    public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
         Product productAdded = productDao.save(product);
         if(productAdded == null)
             return ResponseEntity.noContent().build();
